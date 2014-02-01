adf497d36c9c66743c27c8d3f77749126fee4beb
maddin <admin@maddin.cc>
2014-02-01 23:39:52 +0100

    v1.6.3
    grant unsafeWindow, added description

--- dist/BreadfishPlusPlus.meta.js
+++ dist/BreadfishPlusPlus.meta.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name            Breadfish++
+// @description     Eine Zusammenfassung von erweiterungen für breadfish.de
 // @author          Martin Rump <admin@maddin.cc>
-// @version         1.6.1
+// @version         1.6.3
 // @namespace       http://maddin.cc
-// @include         *://forum.sa-mp.de/*
+// @match          *://forum.sa-mp.de/*
 // @exclude         *://forum.sa-mp.de/acp/*
 // @require         https://code.jquery.com/jquery-1.11.0.min.js
+// @grant           unsafeWindow
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_addStyle
