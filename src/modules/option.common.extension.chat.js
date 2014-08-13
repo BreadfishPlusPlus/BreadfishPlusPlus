9fa47d9dd8f0264cd87f71c7d0cb8547ffba63ec
maddin <admin@maddin.cc>
2014-08-13 11:51:43 +0200

    Chatmodul ist nun wirklich fertig

--- src/modules/option.common.extension.chat.js
+++ src/modules/option.common.extension.chat.js
@@ -20,94 +20,79 @@ register({
     'type': 'invis',
     'default': true,
 });
+register({
+    'key': 'option.boards.extension.ircChat.small',
+    'type': 'invis',
+    'default': false,
+});
 
-if (storage.get('option.common.extension.chat.enabled', false)) {
-    require('./../styles/chat.less');
-    var socket, $msgs, $chat, sendMessage, addMessage, getuserInfo, chatMsgTemplate, createConnection, cachedMsg = {
-        type: 'message'
+var socket, $messages;
+
+var getuserInfo = function (callback) {
+    var userinfo = {
+        name: null,
+        avatar: null,
+        userId: utils.getParameterByName('userID', $("#userNote > a").attr('href'))
     };
+    $.get($("#userNote > a").attr('href')).done(function (data) {
+        var $profile = $(data);
+        userinfo.name = $profile.find('.userName span').text();
+        userinfo.avatar = 'http://forum.sa-mp.de/' + $profile.find('.userAvatar a img').attr('src');
+        callback(userinfo);
+    }).fail(function (jqXHR) {
+        utils.log.error('Konnte eigenes Benutzerprofil nicht aufrufen.', jqXHR.status, jqXHR.statusText);
+        callback(null);
+    });
+};
 
-    chatMsgTemplate = require('templates').chatMessage;
+var sendMessage = function () {
+    if (!socket) {
+        return;
+    }
+    var message = $('.bpp-chat-input').val();
+    $('.bpp-chat-input').val('');
+    if (!message.length) {
+        return;
+    }
+    socket.emit('cmessage', message);
+};
 
-    getuserInfo = function (callback) {
-        var userinfo = {
-            name: null,
-            avatar: null,
-            userId: utils.getParameterByName('userID', $("#userNote > a").attr('href'))
-        };
-        $.get($("#userNote > a").attr('href')).done(function (data) {
-            var $profile = $(data);
-            userinfo.name = $profile.find('.userName span').text();
-            userinfo.avatar = 'http://forum.sa-mp.de/' + $profile.find('.userAvatar a img').attr('src');
-            callback(userinfo);
-        }).fail(function (jqXHR) {
-            utils.log.error('Konnte eigenes Benutzerprofil nicht aufrufen.', jqXHR.status, jqXHR.statusText);
-            callback(null);
-        });
-    };
+var addMessage = function (data) {
+    data.type = data.type || 'message';
+    $messages.append(require('templates').chatMessage(data));
+    $messages[0].scrollTop = $messages[0].scrollHeight;
+};
 
-    sendMessage = function () {
-        if (!socket) {
-            return;
-        }
-        var message = $('.bpp-chat-input').val();
-        $('.bpp-chat-input').val('');
-        if (!message.length) {
-            return;
-        }
-        socket.emit('cmessage', message);
-    };
-
-    addMessage = function (data) {
-        data.type = data.type || 'message';
-        $msgs.append(chatMsgTemplate(data));
-        $msgs[0].scrollTop = $msgs[0].scrollHeight;
-    };
-
-    createConnection = function (userinfo) {
+var connectToSocket = function (userinfo) {
+    $.getScript('https://cdn.socket.io/socket.io-1.0.6.js').done(function () {
         socket = io('http://localhost:1337', { //Wird im Final Release angepasst.
             reconnection: true,
         });
         socket.on('connect', function () {
-            addMessage({
-                type: 'system',
-                message: 'Verbindung wurde hergestellt!'
-            });
+            addMessage({type: 'system', message: 'Verbindung wurde hergestellt!'});
             utils.log.debug('Verbindung wurde hergestellt.');
             socket.emit('userinfo', userinfo);
         });
         socket.on('connect_error', function (err) {
-            addMessage({
-                type: 'system',
-                message: 'Verbindung konnte nicht hergestellt werden! :('
-            });
+            addMessage({type: 'system', message: 'Verbindung konnte nicht hergestellt werden! :('});
             utils.log.error('Die Verbindung zum Chatserver konnte nicht hergestellt werden.', err);
         });
         socket.on('connect_timeout', function () {
-            addMessage({
-                type: 'system',
-                message: 'Deine Verbindung zum Chatserver wurde verloren!'
-            });
+            addMessage({type: 'system', message: 'Deine Verbindung zum Chatserver wurde verloren!'});
             utils.log.error('Deine Verbindung zum Chatserver wurde verloren');
         });
         socket.on('reconnect', function (num) {
             utils.log.debug('Verbindung wurde wiederhergestelllt.', num);
         });
         socket.on('reconnecting', function (num) {
-            addMessage({
-                type: 'system',
-                message: 'Versuche Verbindung wiederherzustellen... (' + num + ')'
-            });
+            addMessage({type: 'system', message: 'Versuche Verbindung wiederherzustellen... (' + num + ')'});
             utils.log.error('Versuche Verbindung wiederherzustellen...', num);
         });
         socket.on('reconnect_error', function (err) {
             utils.log.error('Die Verbindung zum Chatserver konnte nicht wiederhergestellt werden.', err);
         });
         socket.on('reconnect_failed', function () {
-            addMessage({
-                type: 'system',
-                message: 'Konnte verbindung nicht wiederherstellen! :('
-            });
+            addMessage({type: 'system', message: 'Konnte verbindung nicht wiederherstellen! :('});
             utils.log.error('Die Verbindung zum Chatserver konnte nicht wiederhergestellt werden.');
         });
         socket.on('smessage', function (data) {
@@ -115,42 +100,79 @@ if (storage.get('option.common.extension.chat.enabled', false)) {
             addMessage(data);
         });
         socket.on('clearchat', function () {
-            $msgs.empty();
-            addMessage({
-                type: 'system',
-                message: 'Der Chatlog wurde gelöscht!'
-            });
+            $messages.empty();
+            addMessage({type: 'system', message: 'Der Chatlog wurde gelöscht!'});
         });
-    };
-
-    $.getScript('https://cdn.socket.io/socket.io-1.0.6.js').done(function () {
-        $chat = $(require('templates').chat());
-        $(".top5box").before($chat);
-
-        $msgs = $chat.find('.bpp-chat-messages');
-
-        addMessage({
-            type: 'system',
-            message: 'Verbindung wird hergestellt...'
-        });
-
-        $('.bpp-chat-input').keyup(function (event) {
-            if (event.keyCode === 13) {
-                event.preventDefault();
-                sendMessage();
+        socket.on('banned', function (name) {
+            if (name) {
+                addMessage({type: 'system', message: 'Du wurdest von <b>' + name + '</b> gebannt. Du kannst nun keine Nachrichten mehr im Chat verfassen.'});
+            } else {
+                addMessage({type: 'system', message: 'Du wurdest im Chat gebannt. Du kannst keine Nachrichten verfassen.'});
             }
         });
-        $('.bpp-chat-submit').click(function (event) {
-            event.preventDefault();
-            sendMessage();
-        });
-
-        getuserInfo(function (data) {
-            if (data) {
-                createConnection(data);
-            }
+        socket.on('unbanned', function (name) {
+            addMessage({type: 'system', message: 'Du wurdest von <b>' + name + '</b> entbannt. Du kannst nun wieder Nachrichten im Chat verfassen.'});
         });
     }).fail(function (jqXHR) {
         utils.log.error('Konnte socket.io Bilbiothek nicht laden.', jqXHR.status, jqXHR.statusText);
     });
+};
+
+var createChat = function () {
+    var $chat = $(require('templates').chat({
+        open: storage.get('option.common.extension.chat.open', true),
+        small: storage.get('option.common.extension.chat.small', false)
+    }));
+
+    $chat.insertAfter(".headlineContainer");
+
+    $messages = $chat.find('.bpp-chat-messages');
+
+    addMessage({type: 'system', message: 'Verbindung wird hergestellt...'});
+
+    getuserInfo(function (data) {
+        if (data) {
+            connectToSocket(data);
+        }
+    });
+
+    $('.bpp-chat-input').keyup(function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            sendMessage();
+        }
+    });
+    $('.bpp-chat-submit').click(function (event) {
+        event.preventDefault();
+        sendMessage();
+    });
+    $('#bpp-toggleChat').click(function (event) {
+        event.preventDefault();
+        var open = storage.get('option.common.extension.chat.open', true);
+        if (open) {
+            $('.bpp-chat-contents').slideUp();
+        } else {
+            $('.bpp-chat-contents').slideDown();
+        }
+        $(this).find('img').attr('src', !open ? 'wcf/icon/minusS.png' : 'wcf/icon/plusS.png');
+        storage.set('option.common.extension.chat.open', !open);
+    });
+    $('.bpp-toggleSize').click(function (event) {
+        event.preventDefault();
+        if ($('#bpp-chat').hasClass('bpp-chat-small')) {
+            $('#bpp-chat').removeClass('bpp-chat-small').addClass('bpp-chat-large');
+            storage.set('option.common.extension.chat.small', false);
+        } else {
+            $('#bpp-chat').removeClass('bpp-chat-large').addClass('bpp-chat-small');
+            storage.set('option.common.extension.chat.small', true);
+        }
+    });
+};
+
+if (storage.get('option.common.extension.chat.enabled', false)) {
+    if ((utils.isTemplate('tplIndex') && !storage.get('option.common.extension.chat.small', false)) ||
+            storage.get('option.common.extension.chat.small', false)) {
+        require('./../styles/chat.less');
+        createChat();
+    }
 }
