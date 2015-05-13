c53f15efd7dd641244cab85fe80066638c41a196
Martin Rump <hi@maddin.ga>
2015-05-13 19:29:09 +0200

    Modul option.common.filter.gamekeyBanner hinzugefügt

---
+++ src/modules/option.common.filter.gamekeyBanner.js
@@ -0,0 +1,18 @@
+var $           = require('lib/jquery');
+var storage     = require('../storage');
+var register    = require('../settings').register;
+
+register({
+    'key': 'option.common.filter.gamekeyBanner.enabled',
+    'name': 'Breadfish-Gamekeys Banner',
+    'tab': 'Einstellungen',
+    'subtab': 'Allgemeine Einstellungen',
+    'category': 'Filter',
+    'type': 'toggle',
+    'default': false,
+    'description': 'Entfernt den Breadfish-Gamekeys Banner im Footer'
+});
+
+if (storage.get('option.common.filter.gamekeyBanner.enabled', false)) {
+    $('#footer > div[style="text-align: center"]').hide();
+}
