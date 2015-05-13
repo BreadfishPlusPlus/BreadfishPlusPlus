d6bbe5af2c2df68e1370eb62fc7462357221ad62
Martin Rump <hi@maddin.ga>
2015-05-13 22:10:13 +0200

    key mod für bilderzoom

--- src/modules/option.posts.extension.imageResize.js
+++ src/modules/option.posts.extension.imageResize.js
@@ -1,6 +1,7 @@
 var $           = require('lib/jquery');
 var storage     = require('../storage');
 var utils       = require('../utils');
+var KeyboardJS  = require('lib/keyboard');
 var register    = require('../settings').register;
 
 register({
@@ -14,10 +15,46 @@ register({
     'description': 'Erlaubt es, die Größe von Bildern in Signaturen und Posts per Mausrad zu ändern.'
 });
 
-var STEPS = 30;
+
+register({
+    'key': 'option.keyboard.imageResize.modifier',
+    'name': 'Nächste Seite',
+    'tab': 'Tastaturnavigation',
+    'subtab': 'Erweiterungen',
+    'category': 'Bilderzoom Tastenkombination',
+    'type': 'keyboard',
+    'default': -1,
+    'description': 'Wird diese option gesetzt, funktioniert der Bilderzoom (siehe Einstellungen -> Beiträge & Nachrichten -> Erweiterungen) nur noch in verbindung mit dieser Taste + Mausrad.'
+});
+
+var STEPS = 30; //TODO: versteckte option
+
+var isModifierActive = function (modifierKey, keyNames) {
+    utils.log.debug('modifierKey', modifierKey);
+    if (modifierKey === -1) {
+        return true;
+    }
+    utils.log.debug('keyNames', keyNames);
+    var activeKeys = KeyboardJS.activeKeys();
+    utils.log.debug('activeKeys', activeKeys);
+    for(var i=0; i<keyNames.length; i++) {
+        utils.log.debug('indexOf', activeKeys.indexOf(keyNames[i]));
+        if (activeKeys.indexOf(keyNames[i]) === -1) {
+            return false;
+        }
+    }
+    return true;
+};
 
 if (storage.get('option.posts.extension.imageResize.enabled', false) && utils.isTemplate('tplThread')) {
+    var modifierKey = storage.get('option.keyboard.imageResize.modifier', -1);
+    var keyNames = KeyboardJS.key.name(modifierKey);
+
     $(document).on('mousewheel', '.resizeImage,.bpp_resizeImage', function (event) {
+        if (!isModifierActive(modifierKey, keyNames)) {
+            return;
+        }
+
         event.preventDefault();
         var zoom = (event.deltaX || event.deltaY) * STEPS,
             $elem = $(this);
