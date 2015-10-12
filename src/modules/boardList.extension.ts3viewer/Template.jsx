aa415dff06c29c54eea4373cee870e417a3807ed
Martin Rump <contact@maddin.ga>
2015-10-12 02:22:52 +0200

    fix #67

--- src/modules/boardList.extension.ts3viewer/Template.jsx
+++ src/modules/boardList.extension.ts3viewer/Template.jsx
@@ -48,6 +48,7 @@ export default class TS3Viewer extends React.Component {
             });
     }
     getTimeElement(moment) {
+        moment.local();
         return (<time
             className="datetime"
             data-date={moment.format("D. MMMM YYYY")}
