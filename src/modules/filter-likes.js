a6d6ce8f33772df987279a7ceaf0dace213f9fb6
Martin Rump <maddindag77@gmail.com>
2016-09-22 23:50:44 +0200

    Likes ausblenden

--- src/modules/filter-likes.js
+++ src/modules/filter-likes.js
@@ -1,32 +1,74 @@
-import {isTemplate} from "../breadfish";
+import {isTemplate, addDOMinsertedListener} from "../breadfish";
 import $ from "jquery";
 import {get, set} from "../storage";
 import {registerOption} from "../config";
 
-const toggle = ({value=get("filterDeleted", false), init=false} = {}) => {
+const toggle = ({value=get("filterLikes", false), init=false} = {}) => {
     if (!init) {
-        set("filterDeleted", value);
+        set("filterLikes", value);
     }
+    // letzten x beiträge
+    if (isTemplate("boardList")) {
+        if (value) {
+            $(".columnLikes").hide();
+        } else if(!init) {
+            $(".columnLikes").show();
+        }
+    }
+    // themenauflistung
+    if (isTemplate("board")) {
+        if (value) {
+            $(".columnLikes").hide();
+        } else if(!init) {
+            $(".columnLikes").show();
+        }
+    }
+    // posts
     if (isTemplate("thread")) {
         if (value) {
-            $("article.wbbPostDeleted").hide();
+            $(".likesBadge, .likesSummary, .wcfLikeButton").hide();
         } else if(!init) {
-            $("article.wbbPostDeleted").show();
+            $(".likesBadge, .likesSummary, .wcfLikeButton").show();
         }
-    } else if (isTemplate("board")) {
+    }
+    // profil
+    if (isTemplate("user")) {
         if (value) {
-            $(".wbbThreadList > table > tbody > tr.wbbThread.messageDeleted").hide();
+            // sidebar
+            const children = $(".statsDataList").children();
+            children.each((index, element) => {
+                if (element.innerText === "Erhaltene Likes") {
+                    $(element).hide();
+                    children.eq(index - 1).hide();
+                    return false;
+                }
+            });
+            // tab nav, pinnwand kommentare, pinnwand kommentare like button
+            $(`[aria-controls="likes"], .likesBadge, .wcfLikeButton`).hide();
         } else if(!init) {
-            $(".wbbThreadList > table > tbody > tr.wbbThread.messageDeleted").show();
+            $(`[aria-controls="likes"], .likesBadge, .wcfLikeButton`).show();
+            $(".statsDataList > *:hidden").show();
         }
+    }
+    // usercard
+    if (value) {
+        const children = $(".userProfilePreview .userStats:first").children();
+        children.each((index, element) => {
+            if (element.innerText === "Erhaltene Likes") {
+                $(element).hide();
+                children.eq(index + 1).hide();
+                return false;
+            }
+        });
     }
 };
 toggle({init: true});
+addDOMinsertedListener(() => toggle());
 
 registerOption({
     id: "filterLikes",
     name: "Likes ausblenden",
-    tab: ["Posts", "Thread"],
+    tab: "Allgemein",
     type: "toggle",
     default: false,
     description: "Blendet alles was mit Likes zu tun hat aus.",
